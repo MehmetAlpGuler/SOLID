5b7968e1216f71de5484950f9c402cfdb41a04e0
Mehmet Alp <mehmet.guler@iutecredit.com>
2021-01-17 22:43:12 +0200

    edit liskov

---
+++ src/main/java/com/java/principles/solid/liskov/substitution/correct/CanonPrinter.java
@@ -0,0 +1,13 @@
+package com.java.principles.solid.liskov.substitution.correct;
+
+public class CanonPrinter extends BasePrinter implements Scan{
+    @Override
+    public void scan(String value) {
+
+    }
+
+    @Override
+    public void print(String value) {
+
+    }
+}
